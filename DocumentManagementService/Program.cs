3d18a146ed39216c3c2646ccef50f8cbfb6808f0
Qendresa Hajdari <qh43141@ubt-uni.net>
2024-06-10 16:20:09 +0200

    Add logging to the Document management

--- DocumentManagementService/Program.cs
+++ DocumentManagementService/Program.cs
@@ -1,6 +1,17 @@
 using DocumentManagementService.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog(); // Add this line to use Serilog as the logging provider
+
 
 // Add services to the container.
 builder.Services.AddSingleton<DocumentService>();
@@ -25,5 +36,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+
+// Log application start
+try
+{
+    Log.Information("Starting up");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
