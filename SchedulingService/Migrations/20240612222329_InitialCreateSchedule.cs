6526e5d9a124c87cccfd98e3f9700a59bf7e82dc
Qendresa Hajdari <qh43141@ubt-uni.net>
2024-06-13 00:23:55 +0200

    update schedule service

---
+++ SchedulingService/Migrations/20240612222329_InitialCreateSchedule.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SchedulingService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateSchedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ScheduleManagement");
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                schema: "ScheduleManagement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                schema: "ScheduleManagement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ScheduleId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalSchema: "ScheduleManagement",
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ScheduleId",
+                schema: "ScheduleManagement",
+                table: "Events",
+                column: "ScheduleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events",
+                schema: "ScheduleManagement");
+
+            migrationBuilder.DropTable(
+                name: "Schedules",
+                schema: "ScheduleManagement");
+        }
+    }
+}
