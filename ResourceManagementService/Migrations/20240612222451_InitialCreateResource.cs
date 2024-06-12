d26b1d40296cc4fd1789f3a22d6309826228dffa
Qendresa Hajdari <qh43141@ubt-uni.net>
2024-06-13 00:25:12 +0200

    update resource service

---
+++ ResourceManagementService/Migrations/20240612222451_InitialCreateResource.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ResourceManagementService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateResource : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ResourceManagement");
+
+            migrationBuilder.CreateTable(
+                name: "Resources",
+                schema: "ResourceManagement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resources", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Resources",
+                schema: "ResourceManagement");
+        }
+    }
+}
